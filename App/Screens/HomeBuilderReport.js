8ba5b9c25a00bb0d48b0781901e5245879a99a91
Evan Uwao <uwaoevanz@gmail.com>
2022-02-11 08:42:10 +0300

    User interaction fix

--- App/Screens/HomeBuilderReport.js
+++ App/Screens/HomeBuilderReport.js
@@ -3,33 +3,26 @@ import {
     SafeAreaView,
     Text,
     View,
-    Image,
-    FlatList, Platform,
+    FlatList,
     TouchableOpacity,
-    StyleSheet, Alert,
-    Dimensions,
+    Alert,
 } from 'react-native';
 import Colors from '../Theme/Colors';
-import CustomeFonts from '../Theme/CustomeFonts';
-import Style, { HEIGHT } from '../Theme/Style';
+import Style from '../Theme/Style';
 import { Icon } from 'react-native-elements';
-import {
-    validatePhone, validateEmail, validateName, matchPassword,
-    validationempty, validationBlank, validatePassword
-} from '../Common/Validations';
-import { LocalData, Params, Urls } from '../Common/Urls';
+import { validationempty } from '../Common/Validations';
+import { Urls } from '../Common/Urls';
 import { Indicator, showToast, NoData } from '../Common/CommonMethods';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Axios from 'axios'
 import { useIsFocused } from '@react-navigation/native'
 import Moment from 'moment';
+import Card from '../Compoment/Card';
 
 const Home = ({ navigation, route }) => {
 
     const { pr_id, item, index, milestoneArray } = route.params;
     const [isLoding, setLoding] = useState(false);
-    const [loding, setLoding1] = useState(false);
-    const [loding1, setLoding11] = useState(false);
     const [userArray, setuserArray] = useState()
     const isFocused = useIsFocused()
 
@@ -82,7 +75,6 @@ const Home = ({ navigation, route }) => {
             .then(response => {
                 setLoding(false);
                 showToast(response.data.detail + "", "info")
-                // navigation.goBack();
             }).catch(function (error) {
                 setLoding(false);
                 if (error.response) {
@@ -116,6 +108,29 @@ const Home = ({ navigation, route }) => {
             });
         };
 
+        const showAlert = item => {
+            Alert.alert(
+                "Submit",
+                `Are you sure you want to Submit ?`,
+                [
+                    {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancel Pressed"),
+                        style: "cancel"
+                    },
+                    {
+                        text: "OK", onPress: () => {
+                            if(item.status === 'Awaiting feedback'){
+                                return showToast("Awaiting feedback", "error")
+                            } else {
+                            apiCall_approve(item.pk + "")
+                            }
+                        }
+                    }
+                ],
+                { cancelable: true }
+            );
+        }
 
     return (
         <SafeAreaView style={Style.cointainer}>
@@ -130,12 +145,9 @@ const Home = ({ navigation, route }) => {
                     <View style={[{ width: '50%', marginTop: 20, justifyContent: 'center', alignSelf: 'center' }]}>
                         <TouchableOpacity
                             style={{ width: '100%', alignItems: "center" }}
-                            onPress={() => {
-                                navigation.navigate('MessagesProperty', { pr_id: pr_id })
-                            }}
+                            onPress={() => navigation.navigate('MessagesProperty', { pr_id: pr_id })}
                         >
                             <Icon name={'message-bulleted'} type={'material-community'} color={Colors.TheamColor2} size={35} style={{ width: 40, justifyContent: 'flex-start' }} />
-
                             <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.TheamColor3 }]}>Messages</Text>
                         </TouchableOpacity>
                     </View>
@@ -144,88 +156,29 @@ const Home = ({ navigation, route }) => {
                             showsVerticalScrollIndicator={false}
                             data={milestoneArray}
                             renderItem={({ item, index }) => (
-                                <View
-                                    style={{
-                                        flex: 1, borderBottomWidth: 1, borderColor: Colors.divider,
-                                        flexDirection: 'column', paddingVertical: 10, marginTop: 20, backgroundColor: Colors.TheamColor, elevation: 5, margin: 5, paddingVertical: 15, padding: 10, borderRadius: 10
-                                    }}>
-                                    <View style={{ flexDirection: 'row' }} >
-                                        <Text style={[Style.text14, { flex: 1 }]}>{(index + 1)}.  {item.milestone_name}</Text>
-                                    </View>
-
-                                    {item.status == 'approved' ?
-                                        <View style={{ flexDirection: 'row', }}>
-                                            <Text style={[Style.text14, {
-                                                flexShrink: 1, padding: 4, borderColor: Colors.gray_d1, borderWidth: 1, borderRadius: 8, color: Colors.TheamColor4, marginTop: 8
-                                            }]}>{item.status}</Text>
-                                        </View>
-                                        :
-                                        <View style={{ flexDirection: 'row', }}>
-                                            <Text style={[Style.text14, {
-                                                flexShrink: 1, padding: 4, borderColor: Colors.gray_d1, borderWidth: 1, borderRadius: 8, color: '#FB0015', marginTop: 8
-                                            }]}>{item.status}</Text>
-
-                                        </View>
-                                    }
-
-
-                                    <Text style={[Style.text14, { marginTop: 8 }]}>Due {Moment(item._to).format('MMMM DD, YYYY')}</Text>
-
+                                <Card item={item} index={index}>
                                     <View style={{ flexDirection: 'row' }}>
-                                        {/* {item.status == 'approved' ? null : */}
-                                        <View style={[{ flex: 1, height: 40, marginVertical: 3, marginTop: 8, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 10, borderRadius: 4, backgroundColor: item.status == 'approved' ? Colors.gray : Colors.TheamColor3 }]}>
+                                        <View style={[{ flex: 1, height: 40, marginVertical: 3, marginTop: 8, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 10, borderRadius: 4, backgroundColor: item.status === 'completed' ? Colors.gray : Colors.TheamColor3 }]}>
                                             {
                                                 item.status == 'approved' ?
-                                                    <View
-                                                        style={{ width: '100%' }}
-
-                                                    >
-                                                        <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.white }]}>{item.status == 'approved' ? '' : 'Submit'}</Text>
+                                                    <View style={{ width: '100%' }}>
+                                                        <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.white }]}>Submit</Text>
                                                     </View>
-
-
                                                     :
                                                     <TouchableOpacity
+                                                        disabled={item.status === 'completed' ? true : false}
                                                         style={{ width: '100%' }}
-                                                        onPress={() => {
-                                                            if (item.status == 'approved') { }
-                                                            else {
-                                                                Alert.alert(
-                                                                    "Submit",
-                                                                    `Are you sure you want to Submit ?`,
-                                                                    [
-                                                                        {
-                                                                            text: "Cancel",
-                                                                            onPress: () => console.log("Cancel Pressed"),
-                                                                            style: "cancel"
-                                                                        },
-                                                                        {
-                                                                            text: "OK", onPress: () => {
-                                                                                apiCall_approve(item.pk + "")
-                                                                            }
-                                                                        }
-                                                                    ],
-                                                                    { cancelable: true }
-                                                                );
-                                                            }
-
-                                                        }}
+                                                        onPress={() => showAlert(item)}
                                                     >
-                                                <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.white }]}>{item.status == 'approved' ? '' : 'Submit'}</Text>
+                                                        <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.white }]}>{item.status == 'approved' ? '' : 'Submit'}</Text>
                                                     </TouchableOpacity>
-
-
                                             }
 
                                         </View>
                                     </View>
-                                </View>
+                                </Card>
                             )}
-
-                            keyExtractor={(item, index) => index.toString()}
                         />
-
-
                         :
                         <FlatList
                             style={{ marginTop: 6 }}
--- App/Screens/HomeOwnerReport.js
+++ App/Screens/HomeOwnerReport.js
@@ -3,33 +3,24 @@ import {
     SafeAreaView,
     Text,
     View,
-    Image,
-    FlatList, Platform,
+    FlatList, 
     TouchableOpacity,
-    StyleSheet,
-    Dimensions,
 } from 'react-native';
 import Colors from '../Theme/Colors';
-import CustomeFonts from '../Theme/CustomeFonts';
-import Style, { HEIGHT, WIDTH } from '../Theme/Style';
+import Style from '../Theme/Style';
 import { Icon } from 'react-native-elements';
-import {
-    validatePhone, validateEmail, validateName, matchPassword,
-    validationempty, validationBlank, validatePassword
-} from '../Common/Validations';
-import { LocalData, Params, Urls } from '../Common/Urls';
+import { validationempty } from '../Common/Validations';
+import { Urls } from '../Common/Urls';
 import { Indicator, showToast, NoData } from '../Common/CommonMethods';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Axios from 'axios'
 import { useIsFocused } from '@react-navigation/native'
-import Moment from 'moment';
+import Card from '../Compoment/Card';
 
 
 const Home = ({ navigation, route }) => {
     const { pr_id, item , index} = route.params;
     const [isLoding, setLoding] = useState(false);
-    const [loding, setLoding1] = useState(false);
-    const [loding1, setLoding11] = useState(false);
     const [userArray, setuserArray] = useState([])
     const isFocused = useIsFocused()
 
@@ -45,7 +36,6 @@ const Home = ({ navigation, route }) => {
 
     const apiCall_proprtylist = async () => {
         var access = await AsyncStorage.getItem('access')
-        console.log("======access", access)
         setLoding(true);
 
         const headers = {
@@ -56,7 +46,6 @@ const Home = ({ navigation, route }) => {
         Axios.get(Urls.baseUrl + url, { headers })
             .then(response => {
                 setLoding(false);
-                console.log("ractivitieses", response.data);
                 if (response.data != null) { setuserArray(response.data) }
 
             }).catch(function (error) {
@@ -107,10 +96,8 @@ const Home = ({ navigation, route }) => {
         if (validationempty(pk)) {
             url = 'api/activity/decline/' + pk
         }
-        console.log("url", url);
         Axios.get(Urls.baseUrl + url, { headers })
             .then(response => {
-                console.log("res", response);
                 setLoding(false);
                 showToast(JSON.stringify(response.data.detail) + "", "info")
                 navigation.goBack();
@@ -123,174 +110,105 @@ const Home = ({ navigation, route }) => {
 
     };
 
+    let status = item.status
+
     return (
         <SafeAreaView style={Style.cointainer}>
-
             {isLoding ? (
                 <View style={{ width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', }}>
                     <Indicator></Indicator>
                 </View>
             ) : (
                 <View style={{ flex: 1, paddingHorizontal: 15, flexDirection: 'column' }}>
-
-                    <View style={[Style.buttonStyle2, { width: '50%', marginTop: 10, backgroundColor: Colors.white, borderColor: Colors.gray, borderWidth: 2, borderRadius: 25, justifyContent: 'center', alignSelf: 'center' }]}>
+                    <View style={[{ width: '50%', marginTop: 20, justifyContent: 'center', alignSelf: 'center' }]}>
                         <TouchableOpacity
-                            style={{ width: '100%' }}
-                            onPress={() => {
-                                navigation.navigate('MessagesProperty', { pr_id: pr_id })
-                            }}
+                            style={{ width: '100%', alignItems: "center" }}
+                            onPress={() => navigation.navigate('MessagesProperty', { pr_id: pr_id })}
                         >
+                            <Icon name={'message-bulleted'} type={'material-community'} color={Colors.TheamColor2} size={35} style={{ width: 40, justifyContent: 'flex-start' }} />
                             <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.TheamColor3 }]}>Messages</Text>
                         </TouchableOpacity>
                     </View>
 
-                    {validationempty(item) ?
-                        <TouchableOpacity
-                            style={{
-                                flex: 1, borderBottomWidth: 1, borderColor: Colors.divider,
-                                flexDirection: 'column', paddingVertical: 10, marginTop: 20
-                            }}>
-                            <Text style={[Style.text14, {}]}>{(index + 1)}  {item.milestone_name}</Text>
+                    {   
+                        validationempty(item) ?
 
-                            {item.status == 'approved' ?
-                                <View style={{ flexDirection: 'row', }}>
-                                    <Text style={[Style.text14, {
-                                        flexShrink: 1, padding: 4, borderColor: Colors.gray_d1, borderWidth: 1, borderRadius: 8, color: Colors.TheamColor4, marginTop: 8
-                                    }]}>{item.status}</Text>
-                                </View>
-                                :
-                                <View style={{ flexDirection: 'row', }}>
-                                    <Text style={[Style.text14, {
-                                        flexShrink: 1, padding: 4, borderColor: Colors.gray_d1, borderWidth: 1, borderRadius: 8, color: '#FB0015', marginTop: 8
-                                    }]}>{item.status}</Text>
-
-                                </View>
-                            }
-
-
-                            <Text style={[Style.text14, { marginTop: 8 }]}>Due {Moment(item._to).format('MMMM DD, YYYY')}</Text>
-
+                        <Card item={item} index={index}>
                             <View style={{ flexDirection: 'row' }}>
-                                <View style={[Style.buttonStyle22, { flex: 1, marginRight: 8, backgroundColor: Colors.TheamColor4, borderWidth: 0, }]}>
+                                <View style={[Style.buttonStyle22, { flex: 1, marginRight: 8, backgroundColor: status === 'completed' ? Colors.gray : Colors.TheamColor3 }]}>
                                     <TouchableOpacity
+                                        disabled={status === 'completed' ? true : false}
                                         style={{ width: '100%' }}
-                                        onPress={() => {
-                                            apiCall_approve(item.pk + "")
-                                        }}
+                                        onPress={() => apiCall_approve(item.pk + "")}
                                     >
                                         <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.white }]}>Approve</Text>
                                     </TouchableOpacity>
-
                                 </View>
 
                                 <View style={[Style.buttonStyle22, { flex: 1, marginLeft: 8, backgroundColor: Colors.divider }]}>
-
                                     <TouchableOpacity
+                                        disabled={status === 'completed' ? true : false}
                                         style={{ width: '100%' }}
-                                        onPress={() => {
-                                            apiCall_delete(item.pk + "")
-                                        }}
+                                        onPress={() => apiCall_delete(item.pk + "")}
                                     >
                                         <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.TheamColor4 }]}>Request Changes</Text>
                                     </TouchableOpacity>
-
                                 </View>
-
                             </View>
-
-                        </TouchableOpacity>
+                        </Card>
                         :
                         <FlatList
                             style={{ marginTop: 6 }}
                             showsVerticalScrollIndicator={false}
                             data={userArray}
-                            renderItem={({ item, index }) => (
+                            renderItem={({ item }) => (
                                 <TouchableOpacity
                                     style={{ paddingHorizontal: 10, flexDirection: 'column' }}>
-
-                                    {validationempty(item.activities) ?
+                                    {   
+                                        validationempty(item.activities) ?
+                                        
                                         <FlatList
                                             style={{ flex: 1, width: '100%', }}
                                             showsVerticalScrollIndicator={false}
                                             data={item.activities}
                                             renderItem={({ item, index }) => (
-                                                <TouchableOpacity
-                                                    style={{
-                                                        flex: 1, borderBottomWidth: 1, borderColor: Colors.divider,
-                                                        flexDirection: 'column', paddingVertical: 10,
-                                                    }}>
-                                                    <Text style={[Style.text14, {}]}>{(index + 1)}  {item.milestone_name}</Text>
-
-                                                    {item.status == 'approved' ?
-                                                        <View style={{ flexDirection: 'row', }}>
-                                                            <Text style={[Style.text14, {
-                                                                flexShrink: 1, padding: 4, borderColor: Colors.gray_d1, borderWidth: 1, borderRadius: 8, color: Colors.TheamColor4, marginTop: 8
-                                                            }]}>{item.status}</Text>
-                                                        </View>
-                                                        :
-                                                        <View style={{ flexDirection: 'row', }}>
-                                                            <Text style={[Style.text14, {
-                                                                flexShrink: 1, padding: 4, borderColor: Colors.gray_d1, borderWidth: 1, borderRadius: 8, color: '#FB0015', marginTop: 8
-                                                            }]}>{item.status}</Text>
-
-                                                        </View>
-                                                    }
-
-
-                                                    <Text style={[Style.text14, { marginTop: 8 }]}>Due {Moment(item._to).format('MMMM DD, YYYY')}</Text>
-
+                                                <Card item={item} index={index}>
                                                     <View style={{ flexDirection: 'row' }}>
-                                                        <View style={[Style.buttonStyle22, { flex: 1, marginRight: 8, backgroundColor: Colors.TheamColor4, borderWidth: 0, }]}>
-                                                            <TouchableOpacity
+                                                         <View style={[Style.buttonStyle22, { flex: 1, marginRight: 8, backgroundColor: Colors.TheamColor4, borderWidth: 0, }]}>
+                                                             <TouchableOpacity 
                                                                 style={{ width: '100%' }}
-                                                                onPress={() => {
-                                                                    apiCall_approve(item.pk + "")
-                                                                }}
+                                                                onPress={() => apiCall_approve(item.pk + "")}
                                                             >
                                                                 <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.white }]}>Approve</Text>
                                                             </TouchableOpacity>
-
                                                         </View>
 
                                                         <View style={[Style.buttonStyle22, { flex: 1, marginLeft: 8, backgroundColor: Colors.divider }]}>
-
                                                             <TouchableOpacity
                                                                 style={{ width: '100%' }}
-                                                                onPress={() => {
-                                                                    apiCall_delete(item.pk + "")
-                                                                }}
+                                                                onPress={() => apiCall_delete(item.pk + "")}
                                                             >
                                                                 <Text style={[Style.text14, { textAlign: 'center', width: '100%', color: Colors.TheamColor4 }]}>Request Changes</Text>
                                                             </TouchableOpacity>
-
                                                         </View>
-
                                                     </View>
-
-                                                </TouchableOpacity>
-
+                                                </Card>
                                             )}
-                                            keyExtractor={(item, index) => index.toString()}
                                             ListEmptyComponent={<NoData />}
                                         />
-                                        : null}
-
-
+                                        : null
+                                    }
                                 </TouchableOpacity>
                             )}
                             keyExtractor={(item, index) => index.toString()}
-
                         />
                     }
                 </View >
-                // </ScrollView>
             )
             }
         </SafeAreaView >
     );
 };
-
-
 export default Home;
 
 
